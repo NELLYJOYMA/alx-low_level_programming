282a2fab863fe9467af9cb642d2d1ee0c510729e
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-09 04:56:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-!/bin/bash
-/**
-*main-prints a string with puts()
-*
-* Return:null
-* /
+!#/bin/bash
+#include <stdio.h>
 int main(void)
 {
-puts("\ "programming is like building a multilingual puzzle");
-return(0);
+	puts
+		("Programming is like building a multilingual puzzle \n");
+	return(0);
+}	
