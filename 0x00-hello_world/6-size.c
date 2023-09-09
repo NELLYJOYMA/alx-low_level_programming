e914a2c79468702e2275059b1bad758d52fb6957
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-09 08:03:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Return:Always 0
+ */
+int main(void)
+{
+	printf("size of a char:%i byte(s)\n",sizeof(char));
+	printf("size of an int: %i byte(s)\n",sizeof(int));
+	printf("size of a long int:%i byte(s)\n",sizeof(long int));
+	printf("size of a long long int:%i byte(s)\n",sizeof(lon long int));
+	printf("sizee of a float:%i byte(s)\n",sizeof(float));
+return (0);
+}
