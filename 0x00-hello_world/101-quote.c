9e28fc0ad0f5bc940e95b79d9ce762df9663a46b
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-09 09:19:44 -0700

    4-puts.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
-#!/bin/bash
-{#include<stdio.h> 
-init main (void){write(2,"and that piece of art is useful" -Dora Korpar,2015-10-19/n",61);
-return (1);
-}}
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry
+ * Return:1
+ */
+int main(void)
+{
+	Write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 59);
+	return (1);
+}
