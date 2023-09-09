314448380c8db69328f196841d9d8f3160fcc005
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-09 10:44:01 -0700

    positive -ve checker file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-    int n;
-    int ld;
+int n;
+int ld;
 srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    ld = n % 10;
+n = rand() - RAND_MAX / 2;
+ld = n % 10;
 if (ld > 5)
 {
 printf("last digit of %d is %d and is > 5\n", n, ld);
@@ -24,6 +24,6 @@ printf("last digit of %d is %d and is 0\n", n, ld);
 else
 {
 printf("last digit of %d is %d and is < 6 and not 0\n", n, ld);
-}    
+}
 return (0);
 }
