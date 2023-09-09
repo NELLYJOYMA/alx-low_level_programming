a868bf03edc14da276c119df4a3f41ec93bb1069
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-09 06:59:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.>
-/**
- * main -Entry 
- * Return: Always 0
+#include <stdio.h>
 
+
+/**
+ * main -Entry point
+ * Return: Always 0
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
