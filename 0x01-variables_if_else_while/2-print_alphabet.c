76dd7b4e5b956e886ab3d1c3989982a116d2cf5e
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-09 11:55:10 -0700

    creating 2-print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main -Entry point
+ * Retrn:Always 0
+*/
+int main(void)
+{
+int lowercase = 'a';
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase += 1;
+}
+putchar ('\n');
+return (0);
+}
