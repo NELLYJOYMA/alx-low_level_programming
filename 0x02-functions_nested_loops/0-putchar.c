70333f6b61e31565b8a9485f9affcce1ad6ee1e4
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-12 05:22:04 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,14 @@
 /**
  * main -Entry point of my code,
  * Return :Always 0
+*
 */
 int main(void)
 {
+
+
+
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -14,5 +19,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
