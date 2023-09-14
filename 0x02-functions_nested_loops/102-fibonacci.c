d7ed835a43677a7e2edd9f6c274ea9fff964970f
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-13 18:02:09 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - prints the first 52 fibonacci numbers
- * Return: Nothing
+ * Return: Nothing!
  */
 int main(void)
 {
 int i = 0;
-long j =1, k = 1;
-while (i < 52)
+long j =1, k = 2;
+while (i < 50)
 {
 if (i == 0)
 printf("%ld", j);
