5935818217f90696eb2d869840a6cdcecc27c138
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-14 08:38:53 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * more_numbers - prints numbers 0 to 14 10 times
+ * more_numbers - prints numbers 0 to 14 10 times horizontally
  * Return :Always 0
  */
 void more_numbers(void)
@@ -13,7 +13,7 @@ for (j = 0; j <= 14; j++)
 if (j >= 10)
 _putchar('1');
 _putchar(j % 10 + '0');
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
