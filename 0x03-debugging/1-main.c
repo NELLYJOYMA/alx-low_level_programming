e508dc9c7a22a5120bb05fa1c91a78e283e854ff
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-15 14:03:46 -0700

    main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,11 +5,8 @@
 */
 
 int main(void)
-
 {
-
 int i;
-
 printf("Infinite loop incoming :(\n");
 
 i = 0;
@@ -20,7 +17,5 @@ i = 0;
 /*}*/
 
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
-
 }
