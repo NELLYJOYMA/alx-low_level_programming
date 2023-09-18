d231b410c967e2e33529e5edf4836e5ea4952201
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-18 14:34:23 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * _strlen - The function that returns a length of a string
- * @str: The lenght of the  string 
+ * @str: The lenght of the  string
  * Return: the lenght of the @str
  */
 size_t _strlen(const char *str)
