e3773b52b671349edee59032cdfa5442c99aa777
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-21 10:50:03 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp - Compares pointers to two strings
  * @s1: A pointer to the first string to be compared
  * @s2: A pointer to the second string to be compared
- * Return: If str1 < str2, the negative difference of the first unmatched character
+ * Return:If str1 < str2,the negative differenceofthefirstunmatchedcharacter
  * if str1 == str2, 0
  * if str1 > str2, the positive difference of the first unmatched character
  */
