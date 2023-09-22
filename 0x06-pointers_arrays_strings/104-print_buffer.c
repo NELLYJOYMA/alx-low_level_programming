1f7bbfbd086cb65a5088292e131c3e0b714dfe88
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-09-21 18:20:27 -0700

    print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,7 +9,7 @@
  */
 void print_buffer(char *b, int size)
 {
-int o, i, j;
+int o, j, i;
 o = 0;
 
 if (size <= 0)
@@ -19,7 +19,7 @@ return;
 }
 while (o < size)
 {
-j = size - o < 10 ? size - 0 : 10;
+j = size - o < 10 ? size - o : 10;
 printf("%08x: ", o);
 for (i = 0; i < 10; i++)
 {
