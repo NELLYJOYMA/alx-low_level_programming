5c370971f4ec440b83b072a5b013b7ff87119991
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-10-09 14:10:39 -0700

    a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct creating a dog information
+ *
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The dog's owner
+ * Return: Always 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
