c79f1bc8cb3fc59cb080baa6cfc84474cf621524
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-10-11 08:20:35 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * get_op_func - selects the option to perform arithmetic op
+ * @s: operator argument
+ *
+ * Return: result of selected choices
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"_", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL , NULL}
+};
+int i;
+
+/* initialize i */
+i = 0;
+
+/* s does not match any of the operands */
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+return (ops[i].f); /* return operation */
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - sum two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Always 0
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - subtract two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Always 0
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiplies two number
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: ALways 0
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Always 0
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod -  reminder of division
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Always 0
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
