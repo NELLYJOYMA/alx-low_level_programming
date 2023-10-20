1d7a86e8d56612d1faa11564c854591121424fd0
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-10-20 06:24:08 -0700

    a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * first - prints a sentence before main
+ * function es executed
+ *
+ * Return: Always 0
+ */
+void first(void)
+{
+printf("you're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
