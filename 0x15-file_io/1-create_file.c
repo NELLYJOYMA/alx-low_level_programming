925f2bbe5b3516e852688db56d672b6c597ed17f
NELLYJOYMA <makena.nellijoy@gmail.com>
2023-10-30 12:48:56 -0700

    a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - the function creates a file
+ * @filename: The name of the file to be created
+ * @text_content: The content to be copied into the file
+ * Return: 1 if successful and -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+int nletters;
+int rwr;
+if (!filename)
+return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+if (text_content == NULL)
+text_content = "";
+for (nletters = 0; text_content[nletters]; nletters++)
+	;
+rwr = write(fd, text_content, nletters);
+if (rwr == -1)
+return (-1);
+close(fd);
+return (1);
+}
